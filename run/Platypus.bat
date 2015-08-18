363df802c1450fd497b58ed3a4f4609bc866c5e5
Alexey Kashintsev <ak@altsoft.biz>
2015-08-18 14:46:22 +0400

    Added mising jars and modified Platypus.bat

--- run/Platypus.bat
+++ run/Platypus.bat
@@ -1,6 +1,6 @@
 @rem Main section. Don't change
 
-git pull
+@git pull
 
 @set CURRENT_DIR=%~dp0..
 
@@ -11,8 +11,13 @@ git pull
 @set EXT_CLASSES="%CURRENT_DIR%/ext/*";
 @set LAF_CLASS=com.jtattoo.plaf.fast.FastLookAndFeel
 
-@set DB_PROPERTIES="-datasource TSG -dburl jdbc:postgresql://tsg.4rp.org:5432/TSG -dbuser tsg_main -dbpassword tsgpswd -dbschema tsg_main -default-datasource TSG"
-@set APP_PATH="-url jndi://TSG"
-@set APP_ELEMENT="-appelement main_form"
+@set DB_NAME=TSG
+@set DB_URL=jdbc:postgresql://tsg.4rp.org:5432/TSG 
+@set DB_USER=tsg_main
+@set DB_PASSWORD=tsgpswd
+@set DB_SCHEMA=tsg_main
+@set DB_DEF_DS=TSG"
+@set APP_PATH="jndi://TSG"
+@set APP_ELEMENT="main_form"
 
-@java -cp %MAIN_CLASS_JAR%;%EXT_CLASSES% %MAIN_CLASS% %APP_ELEMENT% %DB_PROPERTIES% %APP_PATH% %1 %2
+@start javaw -cp %MAIN_CLASS_JAR%;%EXT_CLASSES% %MAIN_CLASS% -appelement %APP_ELEMENT% -datasource %DB_NAME% -dburl %DB_URL% -dbuser %DB_USER% -dbpassword %DB_PASSWORD% -dbschema %DB_SCHEMA% -default-datasource %DB_DEF_DS% -url %APP_PATH% %1 %2
