7e2067a375875624833e94622c5a5550f2fc8594
Alexey Kashintsev <ak@altsoft.biz>
2015-12-07 22:01:21 +0400

    added user credential information

--- run/Platypus.sh
+++ run/Platypus.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 JRE_PATH=java
-PLATYPUS_HOME=$(cd `dirname $0` && pwd)/..
+PLATYPUS_HOME=".."
 MAIN_CLASS_JAR=$PLATYPUS_HOME/bin/Application.jar
 MAIN_CLASS=com.eas.client.application.PlatypusClientApplication
 EXT_CLASSES=$PLATYPUS_HOME/ext/*
@@ -8,4 +8,4 @@ LAF_CLASS=com.jtattoo.plaf.fast.FastLookAndFeel
 DB_PROPERTIES="-datasource TSG -dburl jdbc:postgresql://tsg.4rp.org:5432/TSG -dbuser tsg_main -dbpassword tsgpswd -dbschema tsg_main -default-datasource TSG"
 APP_PATH="-url jndi://TSG"
 APP_ELEMENT="-appelement main_form"
-$JRE_PATH -cp $MAIN_CLASS_JAR:$EXT_CLASSES $MAIN_CLASS $APP_ELEMENT $DB_PROPERTIES $APP_PATH $1 $2
+$JRE_PATH -cp $MAIN_CLASS_JAR:$EXT_CLASSES $MAIN_CLASS $APP_ELEMENT $DB_PROPERTIES $APP_PATH -user nadB -password masterkey $1 $2
